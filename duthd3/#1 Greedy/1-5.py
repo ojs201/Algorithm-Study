fbbab404da76a382d6700adbf3919679688930b0
duthd3 <duthd3@naver.com>
2023-07-10 14:04:02 +0900

    add 1st week assignment

---
+++ duthd3/#1 Greedy/1-5.py
@@ -0,0 +1,14 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+
+result = 0 #그룹 수
+count = 0 #그룹의 인원 수
+
+for i in data:
+    count += 1
+    if (count >= i):#그룹의 인원이 공포도 이상일 경우
+        result += 1 #그룹결성
+        count = 0 
+print(result)
