f78bf2a38a5570c165667a639c652fde5eb4df3d
LEE-sh1673 <lsh901673@gmail.com>
2023-07-30 14:49:17 +0900

    feat: 4-5까지 완료

--- SeungHoonLee/#4 Sort/4-4.py
+++ SeungHoonLee/#4 Sort/4-4.py
@@ -19,7 +19,7 @@ from sys import stdin
 
 def map_to_name_and_grades(student: str) -> tuple:
     name, kor, eng, math = student.split(' ')
-    return  -int(kor), int(eng), -int(math), name
+    return -int(kor), int(eng), -int(math), name
 
 
 scores = list(map(map_to_name_and_grades, stdin.readlines()[1:]))
